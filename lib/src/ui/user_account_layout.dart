78bfde6f4e74b7df70e80a271b655b9b4a41a22e
NickkTMD <nickk@havocmc.net>
2022-04-21 15:45:08 -0500

    Added upvoty buttons to account section

--- lib/src/ui/user_account_layout.dart
+++ lib/src/ui/user_account_layout.dart
@@ -5,7 +5,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:upvoty/upvoty.dart';
 
+import '../../user_account.dart';
 import '../user_account_service.dart';
 import '../user_account_style.dart';
 import 'user_account_view_badges.dart';
@@ -24,14 +26,24 @@ import 'user_account_view_version.dart';
 
 class UserAccountLayout extends StatelessWidget {
   static const num _cardMarginTop = 2.25;
-  
+
+  static UpvotyStyle style = UpvotyStyle();
+  static Upvoty upvoty = Upvoty(style: style, pressBack: (context) {
+    Navigator.of(context).pop();
+    UserAccount(
+        referalCode: "\$0000T",
+        logout: () => print('logout'),
+        combinedKeys: 'teste')
+        .open(context);
+  });
+
   const UserAccountLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    UserAccountService service =
-        Provider.of<UserAccountService>(context);
+    UserAccountService service = Provider.of<UserAccountService>(context);
     UserAccountStyle style = service.style;
+
     return SizedBox(
         height: style.size(85*8.12),
         child: Column(mainAxisSize: MainAxisSize.min, children: [
@@ -69,6 +81,12 @@ class UserAccountLayout extends StatelessWidget {
                         Container(
                             margin: EdgeInsets.only(top: style.size(_cardMarginTop*8.12)),
                             child: const UserAccountViewSupport()),
+                        Container(
+                            margin: EdgeInsets.only(top: style.size(_cardMarginTop*8.12)),
+                            child: upvoty.buttonSuggestions()),
+                        Container(
+                            margin: EdgeInsets.only(top: style.size(_cardMarginTop*8.12)),
+                            child: upvoty.buttonBugs()),
                         Container(
                             margin: EdgeInsets.only(top: style.size(1.5*8.12)),
                             child: UserAccountViewVersion(style:style)),

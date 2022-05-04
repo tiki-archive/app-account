1bd289a6794151c07140fb91418e75e727218c6b
Ricardo Gonçalves <ricardo@mytiki.com>
2022-05-04 19:23:56 -0300

    refer repository refactor

---
+++ lib/src/refer/ui/code.dart
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tiki_style/tiki_style.dart';
+
+import '../service.dart';
+
+class ReferUiCode extends StatelessWidget {
+  static const String _text = "YOUR CODE:";
+  static const double _fontSize = 14;
+
+  const ReferUiCode({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ReferService service = Provider.of<ReferService>(context);
+    return OutlinedButton(
+        onPressed: () async => service.controller.copyLink(context),
+        style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: Color(0xFFAFAFAF)),
+            primary: ColorProvider.greyThree,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(SizeProvider.instance.width(8))))),
+        child: Padding(padding: EdgeInsets.all(SizeProvider.instance.size(4)), child: Row(mainAxisSize: MainAxisSize.min, children: [
+          Row(mainAxisSize: MainAxisSize.min, children: [
+            Text(_text,
+                style: TextStyle(
+                    fontSize: SizeProvider.instance.text(_fontSize),
+                    fontWeight: FontWeight.bold,
+                    color: ColorProvider.greyFour)),
+            Container(
+                margin: EdgeInsets.all(SizeProvider.instance.width(8)),
+                child: Text(service.code,
+                    style: TextStyle(
+                        fontSize: SizeProvider.instance.text(_fontSize),
+                        fontWeight: FontWeight.bold,
+                        color: const Color(0xFF00133F))))
+          ]),
+          Icon(IconProvider.copy, size: SizeProvider.instance.text(_fontSize), color: ColorProvider.greyFour),
+        ])));
+  }
+}
---
+++ lib/src/refer/ui/refer_share.dart
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tiki_style/tiki_style.dart';
+import '../controller.dart';
+import '../service.dart';
+
+
+class ReferUiShare extends StatelessWidget {
+
+  static const String _text = "SHARE";
+
+  const ReferUiShare({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ReferService service = Provider.of<ReferService>(context);
+    ReferController controller = service.controller;
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: SizeProvider.instance.width(10)),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(SizeProvider.instance.width(80)))),
+            primary: const Color(0xFF27002E)),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                padding: EdgeInsets.only(right: SizeProvider.instance.width(18)),
+                child: Text(_text,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: SizeProvider.instance.text(18),
+                      letterSpacing: SizeProvider.instance.width(2),
+                    ))),
+            Icon(
+              Icons.share,
+              size: SizeProvider.instance.text(22),
+            ),
+          ],
+        ),
+        onPressed: () => controller.onShare());
+  }
+}
